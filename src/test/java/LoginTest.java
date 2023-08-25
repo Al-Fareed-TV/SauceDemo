2c785e2450cf2fb57f32249a6acf947008481c76
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2023-08-25 17:18:04 +0530

    handling all exceptions

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,16 +1,20 @@
-import driver.DriverCreator;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.SessionNotCreatedException;
 import org.testng.annotations.Test;
 import pages.Login;
 
-import static java.lang.Thread.sleep;
 
 public class LoginTest {
+
     @Test
-    public void testLogin()throws InterruptedException {
-        Login login = new Login();
-        login.LoginUser();
+    public void testLogin() throws SessionNotCreatedException {
+        try {
+            Login login = new Login();
+            login.LoginUser();
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
     }
+
 
 }
