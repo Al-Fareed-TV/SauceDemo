15446b047bc49f694e856daf0f0cb2494be32f1f
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2023-08-25 17:10:41 +0530

    user login test

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,17 +1,16 @@
 import driver.DriverCreator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.Login;
 
 import static java.lang.Thread.sleep;
 
 public class LoginTest {
-    String ch = "Hello";
     @Test
     public void testLogin()throws InterruptedException {
-        Assert.assertEquals("Hello",ch);
+        Login login = new Login();
+        login.LoginUser();
     }
 
 }
